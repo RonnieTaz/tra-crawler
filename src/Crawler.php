352a2a47f995b7882c234a978d43fd8c049d0ed8
Vicent Simon <vicsimon.vs4@gmail.com>
2022-10-22 10:00:25 +0300

    :tada: feat(general): Initialize project

---
+++ src/Crawler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ronnie\TRA;
+
+use Symfony\Component\DomCrawler\Crawler as HtmlCrawler;
+
+class  Crawler {
+    private HtmlCrawler $crawler;
+    
+    public function __construct()
+    {
+    }
+}
