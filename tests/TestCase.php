8f4ca341bbdb4a9c33f1c47965a4d5712a49b09e
Vicent Simon <vicsimon.vs4@gmail.com>
2023-10-16 21:46:23 +0300

    :rotating_light: ci(github-actions): Fixed pest tests

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ronnie\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
--- tests/Unit/FetcherTest.php
+++ tests/Unit/FetcherTest.php
@@ -1,23 +1,28 @@
 <?php
 
+use Ronnie\Tests\TestCase;
 use Ronnie\TRA\Contracts\ResourceFetcher;
 use Ronnie\TRA\Fetcher;
 
+uses(TestCase::class);
+
+beforeEach(function () {
+    $fetcher = (new Fetcher())->setUri('https://www.google.com');
+});
+
 it('implements ResourceFetcher', function () {
-    $this->assertInstanceOf(ResourceFetcher::class, new Fetcher());
+    expect(new Fetcher())->toBeInstanceOf(ResourceFetcher::class);
 });
 
 it('returns a fetcher object when setUri is called and its value to match', function () {
-    $this->assertIsObject((new Fetcher())->setUri('https://www.google.com'));
-
-    expect((new Fetcher())->setUri('https://www.google.com'))->toBeObject()
-        ->and((new Fetcher())->setUri('https://www.google.com')->getUri())->toBeString()
-        ->and((new Fetcher())->setUri('https://www.google.com')->getUri())->toEqual('https://www.google.com');
+    $fetcher = (new Fetcher())->setUri('https://www.google.com');
+    expect($fetcher)->toBeObject()
+        ->and($fetcher->getUri())->toBeString()
+        ->and($fetcher->getUri())->toEqual('https://www.google.com');
 });
 
 it('has a defined Uri property if passed in constructor', function () {
-    $this->assertObjectHasAttribute('uri', new Fetcher());
-
-    expect((new Fetcher('https://www.google.com'))->getUri())->toBeString()
+    expect(new Fetcher())->toHaveProperty('uri')
+        ->and((new Fetcher('https://www.google.com'))->getUri())->toBeString()
         ->and((new Fetcher('https://www.google.com'))->getUri())->toEqual('https://www.google.com');
 });
